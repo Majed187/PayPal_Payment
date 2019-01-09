fe7997506a07c2655a409868fd734cf9c3322592
Majed Shrendan <majedshrndan45@gmail.com>
2019-01-09 19:14:58 +0100

    set paypal clint id and secret

--- app.js
+++ app.js
@@ -2,10 +2,63 @@ const express = require("express");
 const ejs = require("ejs");
 const paypal = require("paypal-rest-sdk");
 
+paypal.configure({
+  mode: "sandbox", //sandbox or live
+  client_id:
+    "AaU8tQfmz1_MFDTKuf84yYERXvdDt2ZFJVrxhNW_49DazF4A_F0VBuKyV5_nntyEdZqUa5Oq9ZBj65GV",
+  client_secret:
+    "EAZ8aFDU4lHHLy1bQqULYWqznf3dBknXZW3AH__zFC0bUs8AGUyR6RNbm-jHvqtikX7PsSqMO5vxuvKm"
+});
+
 const app = express();
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.render("index"));
+app.post("/pay", (req, res) => {
+  // this code from the docs of paypal sdk and i hard code it becouse i did't set any  data base  for this sample
+  const create_payment_json = {
+    intent: "sale",
+    payer: {
+      payment_method: "paypal"
+    },
+    redirect_urls: {
+      return_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/cancel"
+    },
+    transactions: [
+      {
+        item_list: {
+          items: [
+            {
+              name: "ps4",
+              sku: "001",
+              price: "250.00",
+              currency: "USD",
+              quantity: 1
+            }
+          ]
+        },
+        amount: {
+          currency: "USD",
+          total: "250.00"
+        },
+        description: "This is the payment description."
+      }
+    ]
+  };
+  // this code from the docs of paypal sdk
+  paypal.payment.create(create_payment_json, function(error, payment) {
+    if (error) {
+      throw error;
+    } else {
+      for (let i = 0; i < payment.links.length; i++) {
+        if (payment.links[i].rel === "approval_url") {
+          res.redirect(payment.links[i].href);
+        }
+      }
+    }
+  });
+});
 
 app.listen(3000, () => console.log("Server Started"));
