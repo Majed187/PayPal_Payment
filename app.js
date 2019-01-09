266b6cb1c1a7acdd535358c56e2130913d5e145e
Majed Shrendan <majedshrndan45@gmail.com>
2019-01-09 19:32:47 +0100

    change some style in view

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ paypal.configure({
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("index"));
 app.post("/pay", (req, res) => {
