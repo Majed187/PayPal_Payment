2b7cde44f88f478463604a0aa18e2229ab55be3e
Majed Shrendan <majedshrndan45@gmail.com>
2019-01-09 19:16:18 +0100

    set route for checking if the payment success  or canseled

--- app.js
+++ app.js
@@ -61,4 +61,37 @@ app.post("/pay", (req, res) => {
   });
 });
 
+app.get("/success", (req, res) => {
+  const payerId = req.query.PayerID;
+  const paymentId = req.query.paymentId;
+
+  const execute_payment_json = {
+    payer_id: payerId,
+    transactions: [
+      {
+        amount: {
+          currency: "USD",
+          total: "250.00"
+        }
+      }
+    ]
+  };
+
+  paypal.payment.execute(paymentId, execute_payment_json, function(
+    error,
+    payment
+  ) {
+    if (error) {
+      console.log(error.response);
+      throw error;
+    } else {
+      console.log(JSON.stringify(payment));
+      res.send("Success payment ");
+    }
+  });
+});
+app.get("cansel", (req, res) => {
+  res.send("Canseled  ");
+});
+
 app.listen(3000, () => console.log("Server Started"));
